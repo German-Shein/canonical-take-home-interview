91e54ac422ed32b923fd40468792b622ad52bad7
German Shein <germanshein1995@gmail.com>
2022-11-30 20:31:55 +0400

    Fixing the unequal height

--- src/script.js
+++ src/script.js
@@ -9,14 +9,18 @@ const posts = fetch ('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/
 		const postElement = document.createElement ("div");
 		postElement.classList.add ("col-4");
 		postElement.classList.add ("col-medium-3");
-		postElement.innerHTML = `<div class="p-card--highlighted rounded-corners top-bar">
-			<h5 class="category-padding">CLOUD AND SERVER</h5>
-			<hr>
-			<div class="p-card__inner">
-				<img class="p-card__image" src="${post.featured_media}">
-				<h4><a href="${post.link}">${post.title.rendered}</a></h4>
-				<h6>By <a href="${post._embedded.author.find (author => author.id === post.author).link}">${post._embedded.author.find (author => author.id === post.author).name}</a> on ${publicationDate.getDate ()} ${publicationDate.toLocaleString ('default', { month: 'long' })} ${publicationDate.getFullYear ()}</h6>
-			</div>
+		postElement.classList.add ("p-card--highlighted");
+		postElement.classList.add ("rounded-corners");
+		postElement.classList.add ("top-bar");
+		postElement.classList.add ("l-card");
+		postElement.innerHTML = `<h5 class="p-muted-heading category-padding">CLOUD AND SERVER</h5>
+		<hr>
+		<div class="p-card__inner">
+			<img class="p-card__image" src="${post.featured_media}">
+			<h4><a href="${post.link}" rel="noreferrer noopener" target="_blank">${post.title.rendered}</a></h4>
+			<h6>By <a href="${post._embedded.author.find (author => author.id === post.author).link}" rel="noreferrer noopener" target="_blank">${post._embedded.author.find (author => author.id === post.author).name}</a> on ${publicationDate.getDate ()} ${publicationDate.toLocaleString ('default', { month: 'long' })} ${publicationDate.getFullYear ()}</h6>
+		</div>
+		<div class="l-footer--sticky">
 			<hr class="u-no-margin--bottom">
 			<div class="p-card__inner">${post.type.slice (0, 1).toUpperCase () + '' + post.type.slice (1, post.type.length)}</div>
 		</div>`;
