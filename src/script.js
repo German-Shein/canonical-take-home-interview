8f02331d4545e6e05717ab092188ec1343f26764
German Shein <germanshein1995@gmail.com>
2022-12-01 15:11:04 +0400

    Finalizing the UI

--- src/script.js
+++ src/script.js
@@ -13,16 +13,16 @@ const posts = fetch ('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/
 		postElement.classList.add ("rounded-corners");
 		postElement.classList.add ("top-bar");
 		postElement.classList.add ("l-card");
-		postElement.innerHTML = `<h5 class="p-muted-heading category-padding">CLOUD AND SERVER</h5>
-		<hr>
-		<div class="p-card__inner">
-			<img class="p-card__image" src="${post.featured_media}">
-			<h4><a href="${post.link}" rel="noreferrer noopener" target="_blank">${post.title.rendered}</a></h4>
-			<h6>By <a href="${post._embedded.author.find (author => author.id === post.author).link}" rel="noreferrer noopener" target="_blank">${post._embedded.author.find (author => author.id === post.author).name}</a> on ${publicationDate.getDate ()} ${publicationDate.toLocaleString ('default', { month: 'long' })} ${publicationDate.getFullYear ()}</h6>
+		postElement.classList.add ("decreased-card-padding");
+		postElement.classList.add ("thicker-shadow");
+		postElement.innerHTML = `<h5 class="card-header">${post.topic.length > 0 ? post._embedded ["wp:term"].flat ().filter (term => term.taxonomy === 'topic').find (term => term.id === post.topic [0]).name.toUpperCase () : 'CLOUD AND SERVER'}</h5>
+		<div class="p-card__inner dotted-horizontal-line decreased_content_padding">
+			<img class="p-card__image rounded-corners" src="${post.featured_media}">
+			<h4><strong><a href="${post.link}" rel="noreferrer noopener" target="_blank">${post.title.rendered}</a></strong></h4>
+			<p><em>By <a href="${post._embedded.author.find (author => author.id === post.author).link}" rel="noreferrer noopener" target="_blank">${post._embedded.author.find (author => author.id === post.author).name}</a> on ${publicationDate.getDate ()} ${publicationDate.toLocaleString ('default', { month: 'long' })} ${publicationDate.getFullYear ()}</em></p>
 		</div>
-		<div class="l-footer--sticky">
-			<hr class="u-no-margin--bottom">
-			<div class="p-card__inner">${post.type.slice (0, 1).toUpperCase () + '' + post.type.slice (1, post.type.length)}</div>
+		<div class="l-footer--sticky dotted-horizontal-line">
+			<p class="p-card__inner card-footer">${post._embedded ["wp:term"].flat ().filter (term => term.taxonomy === 'category').find (term => term.id === post.categories [0]).name.slice (0, -1)}</p>
 		</div>`;
 		document.getElementsByClassName ("row") [0].append (postElement);
 	})
